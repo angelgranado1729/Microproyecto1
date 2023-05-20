7260f5e3e504b5f520f32b0cec817862dffe5159
Angel Granado <angel.granado@correo.unimet.edu.ve>
2023-05-19 20:36:11 -0400

    vamo' a comerrr :construction:

---
+++ app.js
@@ -0,0 +1,13 @@
+const movimientos = document.getElementById("move");
+const tiempo = document.getElementById("time");
+const comenzar = document.getElementById("start");
+const reiniciar = document.getElementById("reset");
+
+let carta_1 = false;
+let carta_2 = false;
+
+const items = [
+    {
+        
+    }
+]
