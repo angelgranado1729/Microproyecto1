e90d663d3b8de1659c461737d43aa3f28124747b
Angel Granado <angel.granado@correo.unimet.edu.ve>
2023-05-20 02:52:34 -0400

    Arregalr detalles :construction:

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
-const movimientos = document.getElementById("move");
 const tiempo = document.getElementById("time");
-const comenzar = document.getElementById("start");
 const reiniciar = document.getElementById("reset");
 const tarjetas = document.querySelectorAll(".tarjeta")
+const comenzar = document.getElementById("start");
+
+
 
 // Tarjetas seleccionadas
 let carta_1;
 let carta_2;
-// auxiliar para que no se pueda seleccionar mas de 2 cartas al mismo tiempo
-let aux = false;
+// Bloquea para que no se pueda seleccionar mas de 2 cartas al mismo tiempo
+let bloquear = false;
+let login = false;
+let nombre = "";
+let tiempo_juego = 0;
+
+// Comienza el juego
+// comenzar.addEventListener("click", () => {
 
 
+// Mezcla las cartas aleatoriamente
+function mezclar() {
+    tarjetas.forEach(tarjeta => {
+        let random = Math.floor(Math.random() * 16);
+        tarjeta.style.order = random;
+    });
+}
 
-//Mezcla las cartas aleatoriamente
-// function mezclar() {
-//     tarjetas.forEach(tarjeta => {
-//         let random = Math.floor(Math.random() * 12);
-//         tarjeta.style.order = random;
-//     });
-// }
+mezclar();
 
-// mezclar();
-
-// // Funcion para comenzar el juego
+// Funcion para comenzar el juego
 // function comenzarJuego() {
 //     comenzar.style.display = "none";
 //     reiniciar.style.display = "block";
 //     let segundos = 0;
 //     let minutos = 0;
-//     let horas = 0;
 //     let tiempo_juego = setInterval(() => {
-//         tiempo.innerHTML = `${horas} : ${minutos} : ${segundos}`;
+//         tiempo.innerHTML = `${minutos} : ${segundos}`;
 //         segundos++;
 //         if (segundos === 60) {
 //             segundos = 0;
@@ -46,10 +51,9 @@ let aux = false;
 //         clearInterval(tiempo_juego);
 //         comenzar.style.display = "block";
 //         reiniciar.style.display = "none";
-//         tiempo.innerHTML = "0 : 0 : 0";
+//         tiempo.innerHTML = "0 : 0";
 //         segundos = 0;
 //         minutos = 0;
-//         horas = 0;
 //         movimientos.innerHTML = 0;
 //         mezclar();
 //         tarjetas.forEach(tarjeta => {
@@ -64,41 +68,48 @@ let aux = false;
 // Compara las imagenes de las tarjetas
 function comparar(carta1, carta2) {
     if (carta1 === carta2) {
-        carta_1.removeEventListener("click", voltear);
+        setTimeout(() => { 
+            carta_1.classList.add("match");
+            carta_2.classList.add("match");
+            carta_1.removeEventListener("click", voltear);
         carta_2.removeEventListener("click", voltear);
-        carta_1 = carta_2 = null;
-        aux = false;
+        }, 300);
+
+        setTimeout(() => {
+            carta_1.classList.remove("match");
+            carta_2.classList.remove("match");
+            carta_1 = carta_2 = null;
+            bloquear = false;
+        }, 750);   
     }else {
         setTimeout(() => {
             carta_1.classList.add("no-match");
             carta_2.classList.add("no-match");
             carta_1.classList.remove("voltear");
             carta_2.classList.remove("voltear");
-        }, 400);
+        }, 300);
     
         setTimeout(() => {
             carta_1.classList.remove("no-match");
             carta_2.classList.remove("no-match");
             carta_1 = carta_2 = null;
-            aux = false;
-        }, 1000);
-    }
-    
-    
+            bloquear = false;
+        }, 750);
+    }      
 }
     
 
 // Funcion para voltear las cartas
 function voltear(e) {
     let carta_selec = e.target;
-    if (carta_selec !== carta_1 && !aux) {
+    if (carta_selec !== carta_1 && !bloquear) {
         carta_selec.classList.add("voltear");
         if (!carta_1) {
             carta_1 = carta_selec;
         } else{
             carta_2 = carta_selec;
             //Bloquea la seleccion de mas cartas
-            aux = true;
+            bloquear = true;
             let img_1 = carta_1.querySelector("img").src;
             let img_2 = carta_2.querySelector("img").src;
             comparar(img_1, img_2);
@@ -112,3 +123,14 @@ tarjetas.forEach(
         tarjeta.addEventListener("click", voltear);
     }
 )
+
+// reiniciar el juego
+reiniciar.addEventListener("click", () => {
+    tarjetas.forEach(tarjeta => {
+        tarjeta.classList.remove("voltear");
+    });
+    mezclar();
+    bloquear = false
+    carta_1 = carta_2 = null;
+}
+);
